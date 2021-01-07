2c95e2fda4f1ab524763b12890bee6a72dfc6e7c
Chamin Jayasooriya <33717507+chamin96@users.noreply.github.com>
2021-01-07 23:21:25 +0530

    Update 23-reg-exp.py

--- 23-reg-exp.py
+++ 23-reg-exp.py
@@ -1,15 +1,31 @@
 # without reg exps
+# file_handle = open('mbox.txt')
+# for line in file_handle:
+#     line = line.rstrip()
+#     if line.find('From:') >= 0:
+#         print(line)
+
 file_handle = open('mbox.txt')
 for line in file_handle:
     line = line.rstrip()
-    if line.find('From:') >= 0:
+    if line.startswith('From:'):
         print(line)
 
 # with reg exps
+# import re
+
+# file_handle = open('mbox.txt')
+# for line in file_handle:
+#     line = line.rstrip()
+#     if re.search('From:', line):
+#         print(line)
+
 import re
 
 file_handle = open('mbox.txt')
 for line in file_handle:
     line = line.rstrip()
-    if re.search('From:', line):
+    if re.search('^From:', line):
         print(line)
+    y = re.findall('\S+@+\S+', line)
+    print(y)
