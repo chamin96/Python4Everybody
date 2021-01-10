f883ee9a58edd12201c649e36523d4d529671896
chamin96 <chaminmax@gmail.com>
2021-01-10 23:59:20 +0530

    bug fix

--- 25-simple-web-browser-urllib.py
+++ 25-simple-web-browser-urllib.py
@@ -1,6 +1,6 @@
 import urllib.request, urllib.parse, urllib.error
 
-file_hand =  urllib.request.urljoin('http://data.pr4e.org/romeo.txt')
+file_hand =  urllib.request.urlopen('http://data.pr4e.org/romeo.txt')
 
 for line in file_hand:
     print(line.decode().strip())
